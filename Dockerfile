1961bd2c14b3bd7bb27997ed70254e558eb3b08d
Michal Malík <97malimi@gmail.com>
2021-07-28 11:47:28 +0200

    ms

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,4 @@
 FROM alpine:3.10
 COPY startmockserver.sh /startmockserver.sh
 ENTRYPOINT ["/startmockserver.sh"]
+
--- startmockserver.sh
+++ startmockserver.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-java -jar mockserver-netty-5.11.2-jar-with-dependencies.jar -serverPort 8080 &
+#java -jar mockserver-netty-5.11.2-jar-with-dependencies.jar -serverPort 8080 &
+echo "bear"
